d09a0b50b8bf19c03197041270fb9b52b59691f9
JoiceStefany <steffanyjoice006@gmail.com>
2023-10-17 23:26:07 -0300

    Email Sender Service

---
+++ src/main/java/com/joicelima/EmailService/application/EmailSenderService.java
@@ -0,0 +1,15 @@
+package com.joicelima.EmailService.application;
+
+
+import com.joicelima.EmailService.core.EmailSenderUseCase;
+
+public class EmailSenderService implements EmailSenderUseCase {
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        throw new UnsupportedOperationException("Unimplemented method 'sendEmail'");
+    }
+    
+
+    
+}
